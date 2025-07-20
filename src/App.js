b1633ef7de036735dc083f7198a1db1886be8854
hain <hainofficialweb@gmail.com>
2025-07-20 23:44:26 +0530

    feat(frontend): setup React Router with pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-     <h2>Welcome to taskify</h2>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
