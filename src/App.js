5b8a7edbbecfdbafff9a109ffb8128bb53044e47
hain <hainofficialweb@gmail.com>
2025-08-09 00:27:44 +0530

    chore(frontend): add tasks route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
+import Tasks from "./pages/Tasks";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/tasks" element={<Tasks />} />
       </Routes>
     </Router>
   );
