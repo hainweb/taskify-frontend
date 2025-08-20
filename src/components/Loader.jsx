c69ee1a794533c99f7ecf9d2d331d95d0a777905
hain <hainofficialweb@gmail.com>
2025-08-21 01:04:29 +0530

    feat(frontend): add loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="flex justify-center items-center p-10">
+      <div className="animate-spin h-10 w-10 border-4 border-blue-500 border-t-transparent rounded-full"></div>
+    </div>
+  );
+}
+
+export default Loader;
